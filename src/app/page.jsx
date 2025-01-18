222a312963c6dc832e3d448ec25268bc6dbe7697
Abdul Hadi <abdulhaadi.76543@gmail.com>
2025-01-18 12:04:49 +0500

    n

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
 import "./globals.css";
-import Header from "../../components/Header.jsx";
-import Hero from "../../components/Hero.jsx";
-import Intro from "../../components/Intro.jsx";
-import Services from "../../components/Services.jsx";
-import Contact from "../../components/Contact.jsx";
-import Footer from "../../components/Footer.jsx";
-import TestimonialSection from "../../components/TestimonialSection.jsx";
+import Header from '../components/Header'
+import Hero from "../components/Hero.";
+import Intro from "../components/Intro";
+import Services from "../components/Services";
+import Contact from "../components/Contact";
+import Footer from "../components/Footer";
+import TestimonialSection from "../components/TestimonialSection";
 import React from 'react';
 
 
