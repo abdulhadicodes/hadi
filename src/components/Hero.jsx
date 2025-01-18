497746dcecc38a11f8e0c2db35bfc1994f139d43
Abdul Hadi <abdulhaadi.76543@gmail.com>
2025-01-18 22:55:36 +0500

    new

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,14 +14,13 @@ function Hero() {
           ></div>
       <div className="bg-opacity-65 inset-0 bg-black absolute"/>
       <div className="relative mx-auto pb-10 px-4 py-24 z-10 lg:p-20 mt-36 lg:ml-52 ml-7">
-      <h1 className="text-5xl font-bold mb-6 animate-fade-in-up text-white lg:text-6xl">Transform your ideas into reality</h1>
+      <h1 className="text-5xl font-bold mb-6 animate-fade-in-up text-white lg:text-6xl">Your Vision, My Expertise</h1>
         <p className="text-3xl mb-8 max-w-2xl animate-fade-in-up animation-delay-200 text-white">
-        Transform your ideas into reality.
+        Turning ideas into real solutions.
         </p>
         <Link href='/services'
         
-        className=
-          "px-6 py-3 text-[#00ff66] bg-transparent border-2 border-[#00ff66]
+        className="px-6 py-3 text-[#00ff66] bg-transparent border-2 border-[#00ff66]
           text-lg font-medium transition-all duration-300
           hover:bg-[#00ff66] hover:text-black hover:shadow-[0_0_20px_rgba(0,255,102,0.4)]
           focus:outline-none focus:ring-2 focus:ring-[#00ff66] focus:ring-offset-2 focus:ring-offset-black"
